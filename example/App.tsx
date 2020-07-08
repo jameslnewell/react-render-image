11d1c249680057549edda9bd5cf23fb728d6322d
James Newell <jameslnewell@users.noreply.github.com>
2020-07-09 09:44:26 +1000

    Added support for srcset (#5)
    * added support for srcset
    
    * use textareas in the example

--- example/App.tsx
+++ example/App.tsx
@@ -1,18 +1,22 @@
 /* eslint-disable jsx-a11y/no-autofocus */
 import React from 'react';
-import Image from '../src';
+import Image, {ImageRendererProps} from '../src';
 
 const defaultURL = 'https://i.pinimg.com/originals/2e/90/0a/2e900a74a9c7e7babe3343fa9dfd6a06.jpg';
 
 const App: React.FC = () => {
-  const input = React.useRef<HTMLInputElement>(null);
-  const [url, setURL] = React.useState(defaultURL);
+  const srcInput = React.useRef<HTMLInputElement>(null);
+  const srcsetInput = React.useRef<HTMLTextAreaElement>(null);
+  const sizesInput = React.useRef<HTMLTextAreaElement>(null);
+  const [props, setProps] = React.useState<Pick<ImageRendererProps, 'src' | 'srcset' | 'sizes'>>({src: defaultURL});
 
   const handleSubmit = (event: React.FormEvent): void => {
     event.preventDefault();
-    if (input.current){
-      setURL(input.current.value);
-    }
+    setProps({
+      src: srcInput.current?.value || '',
+      srcset: srcsetInput.current?.value,
+      sizes: sizesInput.current?.value,
+    });
   };
 
   return (
@@ -21,18 +25,39 @@ const App: React.FC = () => {
 
       <br />
       <form onSubmit={handleSubmit}>
-        <input
-          ref={input}
-          autoFocus
-          defaultValue={defaultURL}
-          style={{width: '340px'}}
-        />
-        <button>load..</button>
-        <Image src={url} loading="🔄" loaded="✅" errored="❌" />
+        <div>
+          <label htmlFor="src">src</label>
+          <input
+            id="src"
+            ref={srcInput}
+            autoFocus
+            defaultValue={defaultURL}
+            style={{width: '340px'}}
+          />
+        </div>
+        <div>
+          <label htmlFor="srcset">srcset</label>
+          <textarea
+            id="srcset"
+            ref={srcsetInput}
+            style={{width: '340px'}}
+          />
+        </div>
+        <div>
+          <label htmlFor="sizes">sizes</label>
+          <textarea
+            id="sizes"
+            ref={sizesInput}
+            autoFocus
+            style={{width: '340px'}}
+          />
+        </div>
+        <button>load...</button>
+        <Image {...props} loading="🔄" loaded="✅" errored="❌" />
       </form>
       <br />
 
-      <Image src={url}>
+      <Image {...props}>
         {({image, loaded, errored}) => {
           return (
             <div>
@@ -46,7 +71,7 @@ const App: React.FC = () => {
               {image && `Height: ${image.height}`}
               <br />
 
-              {loaded && <img src={url} alt="" style={{maxWidth: '100%'}} />}
+              {loaded && <img src={image?.currentSrc} alt="" style={{maxWidth: '100%'}} />}
             </div>
           );
         }}
